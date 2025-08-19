c18af23885485203d8797ccfbc35243cbcdd61da
qgkkk <162648118+qgkkk@users.noreply.github.com>
2025-08-19 11:58:41 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -795,7 +795,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://url.v1.mk" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://25500.423978.xyz:8888" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
@@ -1319,6 +1319,7 @@ export default {
   }
 };
 </script>
+
 
 
 
